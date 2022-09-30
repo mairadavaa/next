819539e7a20ef0dcbba76a7e5fc993f7bba13329
mairadavaa <davaamaira@gmail.com>
2022-09-30 09:49:20 +0800

    111

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,110 @@
-export default function Homes(){
-    return (
-        <div>hello</div>
-    )
-} 
+import Image from "next/image";
+import { Typography, Box, List, Link } from "@mui/material";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+
+import ListItemText from "@mui/material/ListItemText";
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+
+import Button from "@mui/material/Button";
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+export default function Homes(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+  return (
+    <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+      <Image
+        src="https://cdn.discordapp.com/attachments/953598350815535154/1024875162325233684/brooke-cagle-cb4Dv50LN1Y-unsplash_1.jpg"
+        alt="backImage"
+        layout="fill"
+        objectFit="cover"
+      />
+      <Box style={{ position: "absolute" }}>
+        <Box sx={{ display: "flex" }}>
+          <AppBar component="nav">
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+              >
+                MUI
+              </Typography>
+              <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                {navItems.map((item) => (
+                  <Button key={item} sx={{ color: "#fff" }}>
+                    {item}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Box component="nav">
+            <Drawer
+              container={container}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import styles from '../styles/Home.module.css'
 import Homes from './home'
 export default function Home() {
   return (
-      <Homes/>
+    <div className={styles.container}>
+          <Homes/>
+    </div>
   )
 }
